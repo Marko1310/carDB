7134276984022db235a3674c3be693c8c58baab2
Marko1310 <marko.cabo13@gmail.com>
2022-12-18 14:07:47 +0100

    Fetch first 10 000 cars

--- carDB/src/App.js
+++ carDB/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // state for cars database
+  const [cars, setCars] = useState([]);
+
+  // fetch all the cars
+  const getAllCars = function () {
+    fetch("https://parseapi.back4app.com/classes/Car_Model_List?limit=10000", {
+      headers: {
+        "X-Parse-Application-Id": "hlhoNKjOvEhqzcVAJ1lxjicJLZNVv36GdbboZj3Z", // This is the fake app's application id
+        "X-Parse-Master-Key": "SNMJJF0CZZhTPhLDIqGhTlUNV9r60M2Z5spyWfXW", // This is the fake app's readonly master key
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newArr = data.results.flat(2);
+        return setCars(newArr);
+      });
+  };
+
+  // import cars state
+  useEffect(() => getAllCars(), []);
+  console.log(cars);
+
+  return <div></div>;
 }
 
 export default App;
